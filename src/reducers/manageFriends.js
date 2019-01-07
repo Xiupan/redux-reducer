388a3e1c9cd10ceae1c12f4345820e72e81e4983
Alan Hong <xiupan@gmail.com>
2019-01-07 12:09:59 -0600

    Understading the basics.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+  let newState = {...state}
+
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      newState.friends.push(action.friend)
+      break;
+    case 'REMOVE_FRIEND':
+      newState.friends.map((friend, index) => {
+        console.log(friend, index);
+        if (friend.id === action.id) {
+          newState.friends.splice(1, index)
+        }
+      })
+      break;
+    default:
+
+  }
+
+  return newState
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
 export function managePresents(state, action){
+  let newState = {...state}
+
+  switch (action.type) {
+    case 'INCREASE':
+      newState.numberOfPresents += 1
+      break;
+    default:
+
+  }
+
+  return newState
 }
